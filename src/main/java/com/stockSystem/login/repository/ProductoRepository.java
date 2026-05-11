9a0ce8ba80df7e1c9794e717be39d7dc7fff4bf2
meinwissenschaft <kroenenkarl698@gmail.com>
2026-05-10 21:37:41 -0300

    Arreglo de orden en tablas

--- src/main/java/com/stockSystem/login/repository/ProductoRepository.java
+++ src/main/java/com/stockSystem/login/repository/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.stockSystem.login.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface ProductoRepository extends JpaRepository<Producto, Long> {
     Optional<Producto> findByNombre(String nombre);
 
     boolean existsByNombre(String nombre);
+
+    List<Producto> findAllByOrderByCodProdAsc();
 }
