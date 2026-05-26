d5534c16ab1e1b921bac95ee8424e711b50c89e0
meinwissenschaft <kroenenkarl698@gmail.com>
2026-05-26 19:25:41 -0300

    Preparacion de informe

--- src/main/java/com/stockSystem/login/controller/IngresoController.java
+++ src/main/java/com/stockSystem/login/controller/IngresoController.java
@@ -3,7 +3,9 @@ package com.stockSystem.login.controller;
 import com.stockSystem.login.dto.MovimientoRequestDTO;
 import com.stockSystem.login.entity.Ingreso;
 import com.stockSystem.login.service.IngresoService;
+
 import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/stockSystem/login/service/EgresoService.java
+++ src/main/java/com/stockSystem/login/service/EgresoService.java
@@ -4,10 +4,12 @@ import com.stockSystem.login.dto.MovimientoRequestDTO;
 import com.stockSystem.login.entity.Egreso;
 import com.stockSystem.login.entity.Producto;
 import com.stockSystem.login.entity.Stock;
+import com.stockSystem.login.entity.Usuario;
 import com.stockSystem.login.repository.EgresoRepository;
 import com.stockSystem.login.repository.ProductoRepository;
 import com.stockSystem.login.repository.StockRepository;
 
+import com.stockSystem.login.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
@@ -24,6 +26,7 @@ public class EgresoService {
     private final EgresoRepository egresoRepository;
     private final ProductoRepository productoRepository;
     private final StockRepository stockRepository;
+    private final UsuarioRepository usuarioRepository;
 
     public Egreso crearEgreso(MovimientoRequestDTO dto) {
 
@@ -50,5 +53,61 @@ public class EgresoService {
         egreso.setFechaEgreso(LocalDate.now());
 
         return egresoRepository.save(egreso);
+    }
+
+    //registrar el egreso:
+    @Transactional
+
+    public void registrarEgreso(
+            MovimientoRequestDTO dto,
+            String email
+    ) {
+
+        Producto producto = productoRepository
+                .findById(dto.getProductoId())
+                .orElseThrow(() ->
+                        new RuntimeException(
+                                "Producto no encontrado"
+                        )
+                );
+
+        Usuario usuario = usuarioRepository
+                .findByEmail(email)
+                .orElseThrow(() ->
+                        new RuntimeException(
+                                "Usuario no encontrado"
+                        )
+                );
+
+        Stock stock = producto.getStock();
+
+        // validar stock
+        if (stock.getCantidad() < dto.getCantidad()) {
+
+            throw new RuntimeException(
+                    "Stock insuficiente"
+            );
+        }
+
+        // actualizar stock
+        stock.setCantidad(
+                stock.getCantidad() -
+                        dto.getCantidad()
+        );
+
+        stockRepository.save(stock);
+
+        // movimiento
+        Egreso egreso = new Egreso();
+
+        egreso.setProducto(producto);
+
+        egreso.setCantidad(dto.getCantidad());
+
+        egreso.setFechaEgreso(LocalDate.now());
+
+        egreso.setUsuario(usuario);
+
+        egresoRepository.save(egreso);
     }
 }
